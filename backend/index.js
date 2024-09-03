383bd66dd7448ed46f4abab2aa60c18395c65f6e
Petervgine <167954200+Petervgine@users.noreply.github.com>
2024-09-03 22:17:32 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : https://final-ebqrfrontend-real-3g920qfop-peter-ngines-projects.vercel.app,
     credentials : true
 }))
 app.use(express.json())
